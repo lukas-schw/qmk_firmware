283d9741203e7daf90b14dc12cd32b223b67a078
Lukas Schwitzgebel <lukas@schgebel.de>
2019-12-31 16:44:59 +0100

    Add brightness support for custom rgb matrix effect

--- keyboards/massdrop/ctrl/keymaps/custom/rgb_matrix_animations/samurai.h
+++ keyboards/massdrop/ctrl/keymaps/custom/rgb_matrix_animations/samurai.h
@@ -2,13 +2,15 @@ RGB_MATRIX_EFFECT(SAMURAI)
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 bool SAMURAI(effect_params_t* params) {
+    RGB red = hsv_to_rgb((HSV){0, 255, rgb_matrix_config.hsv.v});
+    RGB gold = hsv_to_rgb((HSV){51, 255, rgb_matrix_config.hsv.v});
     // set key leds to red
     for (uint8_t i = 0; i < 87; i++) {
-        rgb_matrix_set_color(i, 0xFF, 0x00, 0x00);
+        rgb_matrix_set_color(i, red.r, red.g, red.b);
     }
     // set underglow to gold
     for (uint8_t i = 87; i < 119; i++) {
-        rgb_matrix_set_color(i, 0xFF, 0xD7, 0x00);
+        rgb_matrix_set_color(i, gold.r, gold.g, gold.b);
     }
 
     return false;
